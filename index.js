5a7c181a8ba0d1592e2281825a5e8ff71d3083c3
Sidharth Bambah <sids53@gmail.com>
2020-03-13 00:26:10 -0400

    Reading from Kafka topic and pushing to EventStream

--- index.js
+++ index.js
@@ -2,24 +2,43 @@ var express = require('express');
 var cors = require('cors');
 var kafka = require('kafka-node');
 
+var Consumer = kafka.Consumer;
+
+// const client = new kafka.KafkaClient({kafkaHost: '35.185.103.25:9092'}); // Sid
+const client = new kafka.KafkaClient({kafkaHost: '35.231.177.25:9092'}); // Jeswanth
+
+
+var topics = [{
+  topic: 'wikipedia_consumer'
+  //topic: 'processed_data'
+}];
+var options = {
+    autoCommit: true,
+    fetchMaxWaitMs: 1000,
+    fetchMaxBytes: 1024 * 1024,
+    encoding: 'utf8'
+  };
+var consumer = new Consumer(client, topics, options);
+
 var app = express();
 app.use(cors());
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('hello world');
   })
 
-// Called once for each new client. Note, this response is left open!
-app.get('/wikipedia_counter', function (req, res) {
-    req.socket.setTimeout(Number.MAX_VALUE);
-    while(True){
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream', // <- Important headers
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
-        });
-        res.write('Sample message');
-    }
-});
+app.get('/wikipedia_counter', function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    consumer.on('message', function(message){
+        var data = message['value'];
+        console.log(data);
+        res.write("data: " + data + "\n\n");
+    });
+})
 
 app.listen(process.env.PORT || 8080)
