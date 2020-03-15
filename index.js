66ae6424e51e30de38fd2d9980d845214ab59e80
Sidharth Bambah <sids53@gmail.com>
2020-03-14 21:19:06 -0400

    Finished NodeJS backend, connected to MongoDB, serves EventSource to React frontend

--- index.js
+++ index.js
@@ -1,44 +1,175 @@
 var express = require('express');
 var cors = require('cors');
 var kafka = require('kafka-node');
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+require('events').EventEmitter.defaultMaxListeners = 100
 
+//Kafka Configuration
 var Consumer = kafka.Consumer;
 
-// const client = new kafka.KafkaClient({kafkaHost: '35.185.103.25:9092'}); // Sid
-const client = new kafka.KafkaClient({kafkaHost: '35.231.177.25:9092'}); // Jeswanth
+const client = new kafka.KafkaClient({kafkaHost: '35.185.103.25:9092'}); // Sid
+//const client = new kafka.KafkaClient({kafkaHost: '35.231.177.25:9092'}); // Jeswanth
 
 
 var topics = [{
-  topic: 'wikipedia_consumer'
-  //topic: 'processed_data'
+  topic: 'processed'
 }];
+
 var options = {
-    autoCommit: true,
-    fetchMaxWaitMs: 1000,
+    fetchMaxWaitMs: 10000,
     fetchMaxBytes: 1024 * 1024,
-    encoding: 'utf8'
-  };
+    encoding: 'utf8',
+    requestTimeout: false
+};
 var consumer = new Consumer(client, topics, options);
 
+//MongoDB Configuration
+var credentials = JSON.parse(fs.readFileSync('./credentials.config', 'utf8'));
+const uri = "mongodb+srv://" + credentials['USERNAME'] + ":" + credentials['PASSWORD'] + "@" + credentials['HOST'] + "/" + credentials['DATABASE'] + "?retryWrites=true&w=majority";
+
+//Express Configuration
 var app = express();
 app.use(cors());
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world');
-  })
+  });
+/*
+consumer.on('message', function(message){
+  console.log(JSON.parse(message['value']));
+})*/
+app.get('/domainCountDonut', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('domainCountDonut')){
+      res.write("data: " + JSON.stringify(data['domainCountDonut']) + "\n\n");
+    }
+  });
+});
 
-app.get('/wikipedia_counter', function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
+app.get('/keyChangesCard', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('keyChangesCard')){
+      res.write("data: " + JSON.stringify(data['keyChangesCard']) + "\n\n");
+    }
+  });
+});
+
+app.get('/botPercent', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('botPercent')){
+      res.write("data: " + JSON.stringify(data['botPercent']) + "\n\n");
+    }
+  });
+});
+
+app.get('/topWikiCards', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('topWikiCards')){
+      res.write("data: " + JSON.stringify(data['topWikiCards']) + "\n\n");
+    }
+  });
+});
+
+app.get('/userLeaderboard', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('userLeaderboard')){
+      res.write("data: " + JSON.stringify(data['userLeaderboard']) + "\n\n");
+    }
+  });
+});
+
+app.get('/additionLineChart', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('additionLineChart')){
+      res.write("data: " + JSON.stringify(data['additionLineChart']) + "\n\n");
+    }
+  });
+});
+
+app.get('/deletionLineChart', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('deletionLineChart')){
+      res.write("data: " + JSON.stringify(data['deletionLineChart']) + "\n\n");
+    }
+  });
+});
+
+app.get('/noeditLineChart', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  consumer.on('message', function(message){
+    var data = JSON.parse(message['value']);
+    if (data.hasOwnProperty('noeditLineChart')){
+      res.write("data: " + JSON.stringify(data['noeditLineChart']) + "\n\n");
+    }
+  });
+});
+
+app.get('/hourlyChangesBarChart', function(req, res){
+  MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("wikiStats");
+    dbo.collection("daywise_changes").findOne({day: req.query['day']}, {projection:{_id:0, day:0}}, function(err, result) {
+      if (err) throw err;
+      if (result != null){
+        formatted = {"labels": Object.keys(result), "data": Object.values(result)};
+        res.send(formatted);
+      } else {
+        res.send({"error": "Day of week not found in database."});
+      }
+      db.close();
     });
-    consumer.on('message', function(message){
-        var data = message['value'];
-        console.log(data);
-        res.write("data: " + data + "\n\n");
-    });
+  });
 })
 
 app.listen(process.env.PORT || 8080)
